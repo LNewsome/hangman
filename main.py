f7729f3c179064f2d87117f60d6d906f0ddad6fa
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2021-06-07 16:27:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
-answer = input("Pleas enter your word for player 2: ")
-answerlist = []
+def playerChoice():
+    number = int(input("Are you player 1 or player 2? "))
+    if number != 1 and number != 2:
+        print("You are player " + number)
 
-if len(answer) > 1 and answer.isalpha():
-    answerlist.append(answer)
-    answer = list(answerlist[0])
-    display = []
-    used = []
-    incorrect = []
-    used.extend(display)
-    display.extend(answer)
-    for i in range(len(display)):
-        display[i] = "_"
-    print(' '.join(display))
-    print()
-    count = 0
 
-    while count > len(answer) and incorrect > 0:
-        incorrect = 0
-        count = 0
-        guess = input("Guess a letter: ")
-        guess = guess.lower()
-        print(count)
+def hangMan():
+    playerChoice()
+    answer = input("Please enter your word for player 2: ")
+    answerlist = []
+
+    if len(answer) > 1 and answer.isalpha():
+        answerlist.append(answer)
+        answer = list(answerlist[0])
+        display = []
+        used = []
+        incorrect = []
+        used.extend(display)
+        display.extend(answer)
+        for i in range(len(display)):
+            display[i] = "_"
+        print(' '.join(display))
         print()
+        count = 0
 
-        for i in range(len(answer)):
-            if answer[i] == guess and guess in used:
-                display[i] = guess
-                count = count + 1
+        while count > len(answer) and incorrect > 0:
+            incorrect = 0
+            count = 0
+            guess = input("Guess a letter: ")
+            guess = guess.lower()
+            print(count)
+            print(answerlist)
 
-                used.remove(guess)
+            for i in range(len(answer)):
+                if answer[i] == guess and guess in used:
+                    display[i] = guess
+                    count = count + 1
 
-        if guess not in display:
-            incorrect = incorrect - 1
-            print("Wrong guess.")
+                    used.remove(guess)
+
+            if guess not in display:
+                incorrect = incorrect - 1
+                print("Wrong guess.")
+            else:
+                print("You ran out of guesses!")
         else:
-            print("You ran out of guesses!")
-    else:
-        print("You need a word!")
+            print("You need a word!")
+
+
+if __name__ == '__main__':
+    hangMan()
+
 
